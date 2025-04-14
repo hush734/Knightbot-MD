31528cbb76f30f9016683c72e15a8d80d9fbff6f
hush734 <basseyhero796@gmail.com>
2025-04-14 02:34:40 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: ': D̶͔̭̪̻ỉ͔͖̜͌åb̬͖̏́͢lø lore',
+  packname: ': D̶͔̭̪̻ỉ͔͖̜͌åb̬͖̏́͢lø is gonna make you squirt',
   author: ': D̶͔̭̪̻ỉ͔͖̜͌åb̬͖̏́͢lø',
   botName: ": D̶͔̭̪̻ỉ͔͖̜͌åb̬͖̏́͢lø Bot",
   botOwner: ': D̶͔̭̪̻ỉ͔͖̜͌åb̬͖̏́͢lø', // Your name
   ownerNumber: '07879371046', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
